168066d337548b5725b52e75489e1e78cb8c05df
Davit Dzneladze <dzneladze0802@gmail.com>
2024-08-05 17:41:41 +0200

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+
+  console.log(`App started at port ${PORT}`);
 }
+
 bootstrap();
